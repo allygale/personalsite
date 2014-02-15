e55180229357fd700a3ea42f7bf9adced9ce251a
Ally Gale <allygale@gmail.com>
2014-02-15 21:04:50 +0100

    now supporting a single, local blog post

--- client/script.js
+++ client/script.js
@@ -1,4 +1,18 @@
+var blogLoaded = false;
 
+function loadBlog() {
+	var xhReq = new XMLHttpRequest();
+ 	xhReq.open("GET", "/posts/happy.txt", true);
+ 	xhReq.onreadystatechange = function() {
+ 		//markdown.toHTML(input.value);
+ 		if (xhReq.readyState == XMLHttpRequest.DONE && xhReq.status == 200) {
+ 			var blogContent = document.querySelector("#blogContent");
+ 			blogContent.innerHTML = markdown.toHTML(xhReq.responseText); 
+ 		}
+ 	};
+ 	xhReq.send();
+ 	blogLoaded = true;
+}
 
 var allygale = document.querySelector("#allygale");
 var interests = document.querySelector("#interests");
@@ -28,4 +42,9 @@ projects.onclick = function(e) {
 blog.onclick = function(e) {
 	hidePages();
 	blogPage.style.display="block";
+	if (!blogLoaded) {
+		console.log(blogLoaded);
+		loadBlog();
+	}
+	console.log(blogLoaded);
 }
